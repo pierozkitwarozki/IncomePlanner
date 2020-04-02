79ea985e8f4f78c3c102fc8cf48dc99cbb09e273
pierozkitwarozki <46139140+pierozkitwarozki@users.noreply.github.com>
2020-04-02 14:59:08 +0200

    'finished_app'

--- IncomePlanner/MainActivity.cs
+++ IncomePlanner/MainActivity.cs
@@ -23,6 +23,8 @@ namespace IncomePlanner
         Button calculateButton;
         RelativeLayout resultLayout;
 
+        bool inputCalculeted = false;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);           
@@ -34,11 +36,46 @@ namespace IncomePlanner
 
         private void CalculateButton_Click(object sender, System.EventArgs e)
         {
+            if (inputCalculeted)
+            {
+                inputCalculeted = false;
+                ClearInput();
+                calculateButton.Text = "Calculate";
+                return;
+            }
             //Take inputs from user
             double incomePerHour = double.Parse(incomePerHourEditText.Text);
             double workHoursPerDay = double.Parse(workHoursEditText.Text);
             double taxRate = double.Parse(taxRateEditText.Text);
             double savingsRate = double.Parse(savingsRateEditText.Text);
+
+            //Caluclate Income, etc...
+            double annualWorkHourSummary = workHoursPerDay * 5 * 50; //5 days a week, 50 weeks a year (two weeks off)
+            double annualIncome = annualWorkHourSummary * incomePerHour;
+            double taxPayable = (taxRate / 100) * annualIncome;
+            double annualSavings = (savingsRate / 100) * annualIncome;
+            double spendableIncome = annualIncome - taxPayable - annualSavings;
+
+            //Display values and set Visible
+            workSummaryValue.Text = annualWorkHourSummary.ToString("#,##") + " HRS";
+            grossIncomeValue.Text = annualIncome.ToString("#,##") + " USD";
+            taxPayableValue.Text = taxPayable.ToString("#,##") + " USD";
+            savingsValue.Text = annualSavings.ToString("#,##") + " USD";
+            spendableIncomeValue.Text = spendableIncome.ToString("#,##") + " USD";
+            resultLayout.Visibility = Android.Views.ViewStates.Visible;
+
+            inputCalculeted = true;
+            calculateButton.Text = "Clear";
+        }
+
+        private void ClearInput()
+        {
+            incomePerHourEditText.Text = "";
+            workHoursEditText.Text = "";
+            taxRateEditText.Text = "";
+            savingsRateEditText.Text = "";
+
+            resultLayout.Visibility = Android.Views.ViewStates.Invisible;
         }
 
         private void ConnectViews()
